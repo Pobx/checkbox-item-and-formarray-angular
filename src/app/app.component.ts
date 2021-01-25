2a03f16502f6c2f5bc0f096a01403ab83a44bd02
Pobx <geidtiphong.@gmail.com>
2021-01-25 22:11:28 +0700

    initialize project

---
+++ src/app/app.component.ts
@@ -0,0 +1,117 @@
+import { Component, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup } from '@angular/forms';
+import { Item } from './item.interface';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent implements OnInit {
+  items: Item[];
+  checkedNumber: number;
+  checkedItems: number[];
+
+  checkedNumberFormArray: number;
+  checkedFormArray: number[];
+  myForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {}
+
+  // tslint:disable-next-line: typedef
+  ngOnInit() {
+    this.items = [];
+    this.checkedItems = [];
+    this.checkedNumber = 0;
+    this.checkedNumberFormArray = 0;
+    this.checkedFormArray = [];
+
+    this.myForm = this.fb.group({
+      entitiesForm: this.fb.array([]),
+    });
+  }
+
+  get formArray(): FormArray {
+    return this.myForm.get(`entitiesForm`) as FormArray;
+  }
+
+  doIncreaseItem() {
+    this.items.push({
+      Name: new Date().toString(),
+      CheckedId: this.checkedNumber,
+    });
+    this.checkedNumber++;
+  }
+
+  doAddChecked(event, index: number) {
+    const isChecked = event.target.checked;
+    const data: Item = this.items[index];
+    if (isChecked) {
+      this.checkedItems.push(data.CheckedId);
+    } else {
+      this.checkedItems = this.checkedItems.filter(
+        (value) => value !== data.CheckedId
+      );
+    }
+  }
+
+  doRemoveItems() {
+    this.checkedItems.forEach((value) => {
+      const index = this.items.map((data) => data.CheckedId).indexOf(value);
+      this.items.splice(index, 1);
+    });
+
+    this.checkedItems = [];
+  }
+
+  doRemoveItemsAll() {
+    this.items = [];
+    this.checkedItems = [];
+  }
+
+  doIncreaseFormArray() {
+    const Entity = this.fb.group({
+      Name: new Date().toString(),
+      CheckedId: this.checkedNumberFormArray,
+    });
+
+    this.formArray.push(Entity);
+    this.checkedNumberFormArray++;
+  }
+
+  doAddCheckedFormArray(event, index: number) {
+    const isChecked = event.target.checked;
+    const formGroup = this.formArray.at(index) as FormGroup;
+
+    const data: Item = formGroup.getRawValue();
+
+    if (isChecked) {
+      this.checkedFormArray.push(data.CheckedId);
+    } else {
+      this.checkedFormArray = this.checkedFormArray.filter(
+        (value) => value !== data.CheckedId
+      );
+    }
+  }
+
+  doRemoveFormArray() {
+    const items: Item[] = this.formArray.getRawValue();
+    console.log(items);
+    this.checkedFormArray.forEach((value) => {
+      const index = this.formArray.controls
+        .map((data) => data.get(`CheckedId`).value)
+        .indexOf(value);
+      this.formArray.removeAt(index);
+    });
+
+    this.checkedFormArray = [];
+  }
+
+  removeAllFomArray() {
+    while (this.formArray.length) {
+      this.formArray.removeAt(0);
+    }
+
+    this.checkedFormArray = [];
+  }
+}
